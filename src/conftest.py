f1d622590967e91c1b39d2e5ee1f4c20b1627a90
Jaime <jaime5alvarez5h@gmail.com>
2025-11-08 11:00:34 +0100

    Remove docker-compose.yml and run-tests.sh; introduce PostgreSQL container fixture in conftest.py for improved test database management.

--- src/conftest.py
+++ src/conftest.py
@@ -1,12 +1,29 @@
 import pytest_asyncio
 from src.modules.shared.database.sql_alchemy_db import DatabaseSessionManager, Base
+from testcontainers.postgres import PostgresContainer
+
+# before all tests
+@pytest_asyncio.fixture(scope="session")
+async def database_url():
+    """Provides a PostgreSQL container for the entire test session"""
+    container = PostgresContainer("postgres:18-alpine")
+    container.start()
+
+    # Replace the connection URL to use asyncpg instead of psycopg2, only if needed
+    yield (
+        container.get_connection_url()
+        .replace("postgresql://", "postgresql+asyncpg://")
+        .replace("postgresql+psycopg2://", "postgresql+asyncpg://")
+    )
+    container.stop()
 
 
+# before each test
 @pytest_asyncio.fixture(scope="function")
-async def db_session():
+async def db_session(database_url):
     """Provides a clean database session for each test"""
     session_manager = DatabaseSessionManager(
-        "postgresql+asyncpg://postgres:postgres@localhost:5432/postgres",
+        database_url,
         {"echo": False},
     )
 
