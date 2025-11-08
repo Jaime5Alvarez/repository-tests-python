ef57f28f4cb554a8b79b590ade3993477e064171
Jaime <jaime5alvarez5h@gmail.com>
2025-11-08 11:01:31 +0100

    Enhance database_url fixture in conftest.py with comments for clarity; specify container start and stop phases for better test management.

--- src/conftest.py
+++ src/conftest.py
@@ -2,10 +2,10 @@ import pytest_asyncio
 from src.modules.shared.database.sql_alchemy_db import DatabaseSessionManager, Base
 from testcontainers.postgres import PostgresContainer
 
-# before all tests
 @pytest_asyncio.fixture(scope="session")
 async def database_url():
     """Provides a PostgreSQL container for the entire test session"""
+    # before all tests
     container = PostgresContainer("postgres:18-alpine")
     container.start()
 
@@ -15,6 +15,7 @@ async def database_url():
         .replace("postgresql://", "postgresql+asyncpg://")
         .replace("postgresql+psycopg2://", "postgresql+asyncpg://")
     )
+    # after all tests
     container.stop()
 
 
